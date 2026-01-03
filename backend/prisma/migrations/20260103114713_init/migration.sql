1fac14dfcc9cba377373ed168c0943826cd7057c
bamekakenang <bamekakenang0@gmail.com>
2026-01-03 13:15:33 +0100

    update

---
+++ backend/prisma/migrations/20260103114713_init/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN "address" TEXT;
+ALTER TABLE "users" ADD COLUMN "city" TEXT;
+ALTER TABLE "users" ADD COLUMN "country" TEXT;
+ALTER TABLE "users" ADD COLUMN "gender" TEXT;
+ALTER TABLE "users" ADD COLUMN "phone" TEXT;
